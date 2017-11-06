00584f1d2face153b4eed2d76dd40fb1b8afa91c
Gonzalo Silverio <gsilver@users.noreply.github.com>
2017-11-06 12:51:35 -0500

    ang-shell - create stubs for all views and add angular seed app and controller (#5)

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+var webGateway = angular.module( "webGateway", ['ngRoute'] );
+
+webGateway.run(function($rootScope) {
+});
+
+webGateway.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: 'src/views/home.html',
+      controller: 'homeController'
+    }).
+    when('/services', {
+      templateUrl: 'src/views/services.html',
+      controller: 'servicesController'
+    }).
+    when('/pricing', {
+      templateUrl: 'src/views/pricing.html',
+      controller: 'pricingController'
+    }).
+    when('/support', {
+      templateUrl: 'src/views/support.html',
+      controller: 'supportController'
+    }).
+    when('/order', {
+      templateUrl: 'src/views/order.html',
+      controller: 'orderController'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+  }
+]);
---
+++ src/js/controllers.js
@@ -0,0 +1,19 @@
+webGateway.controller('homeController', ['$rootScope', '$scope', '$filter', '$timeout', '$log', function($rootScope, $scope, $filter, $timeout, $log) {
+  console.log('home');
+}]);
+
+webGateway.controller('servicesController', ['$rootScope', '$scope', '$filter', '$timeout', '$log', function($rootScope, $scope, $filter, $timeout, $log) {
+  console.log('services');
+}]);
+
+webGateway.controller('pricingController', ['$rootScope', '$scope', '$filter', '$timeout', '$log', function($rootScope, $scope, $filter, $timeout, $log) {
+  console.log('pricing');
+}]);
+
+webGateway.controller('supportController', ['$rootScope', '$scope', '$filter', '$timeout', '$log', function($rootScope, $scope, $filter, $timeout, $log) {
+  console.log('support');
+}]);
+
+webGateway.controller('orderController', ['$rootScope', '$scope', '$filter', '$timeout', '$log', function($rootScope, $scope, $filter, $timeout, $log) {
+  console.log('order');
+}]);
