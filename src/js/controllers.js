bbf64f2150aa3d46805931e4de81f4f8e04614ab
Gonzalo Silverio <gsilver@users.noreply.github.com>
2017-11-22 13:10:42 -0500

    ID81 - validation and debug (#16)

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -15,7 +15,9 @@ webGateway.controller('supportController', ['$rootScope', '$scope', '$filter', '
 }]);
 
 webGateway.controller('orderController', ['$rootScope', '$scope', '$filter', '$timeout', '$log', function($rootScope, $scope, $filter, $timeout, $log) {
-  console.log('order');
+  $scope.validation = {
+    url:null
+  };
   $scope.orderModel = {
     site: null,
     env:null,
@@ -28,4 +30,55 @@ webGateway.controller('orderController', ['$rootScope', '$scope', '$filter', '$t
     mcomm:null,
     free:null
   };
+  $scope.resetOrder = function(val){
+    $scope.orderModel = {
+      site: val,
+      env:null,
+      db:null,
+      url:null,
+      shortcode: null,
+      itar:null,
+      free:null
+    };
+  };
+  $scope.validateUrl = function(){
+    // an http request from some service will return true or false for available or not
+    // here we are checking on the existence of the string 'not' to mimick this
+    if( /[^a-zA-Z0-9]/.test( $scope.orderModel.url ) ) {
+       $scope.validation.url = false; $scope.validation.url_reason = 'Only alphanumeric please.';
+    } else {
+      if($scope.orderModel.url.includes('taken')){
+        $scope.validation.url = false;$scope.validation.url_reason = 'Prefix has already been taken.';
+      } else {
+        $scope.validation.url = true;
+      }
+    }
+  };
+  $scope.validateShortCode = function(){
+    // an http request via ESB will return metadata that we display, we
+    // have no way to validate that the current user has permission to use the shortcode
+    // the metadata is there for self-validation
+    if( /[^0-9]/.test( $scope.orderModel.shortcode ) ) {
+       $scope.validation.shortcode = false; $scope.validation.shortcode_reason = 'Only numeric please.';
+    } else {
+      if($scope.orderModel.shortcode.includes('0')){
+        $scope.validation.shortcode = false;
+        $scope.validation.shortcode_reason = 'Shortcode does not exist.';
+      } else {
+        $scope.validation.shortcode_metadata = {
+          'Shortcode':	'185817 - ITS Teach & Learn - Gen Exp (11088)',
+          'Shortcode Status':	'Open',
+          'Business Unit':	'UMICH',
+          'Fund':	'10000 - General',
+          'DeptID':	'481094 - ITS Teaching and Learning',
+          'Program':	'11088 - General Expense',
+          'Class-icr':	'66000 - Admin Computing Support'
+        };
+        $scope.validation.shortcode = true;
+      }
+    }
+  };
+
+
+
 }]);
